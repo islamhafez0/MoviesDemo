b126a8dd5e36cf6d0229c320012316a976b1be02
islam hafez <aboeshaeslam2@gmail.com>
2023-10-16 19:30:45 -0700

    edit notfound page

--- src/pages/404/Notfound.jsx
+++ src/pages/404/Notfound.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./style.scss";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 const Notfound = () => {
-  return <div>Notfound</div>;
+  return (
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default Notfound;
